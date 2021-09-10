553ce6a3731660a8663927fd3b30b9db7ac357e6
Cavit İbrahim Gökdoğan <70701456+cavitgokdogan@users.noreply.github.com>
2021-09-10 17:39:55 +0300

    Create ilkgorev.java

---
+++ ilkgorev.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ilk_Gorev {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Lutfen matematik notunuzu giriniz: ");
+        int matematik = input.nextInt();
+
+        System.out.print("Lutfen turkce notunuzu giriniz: ");
+        int turkce = input.nextInt();
+
+        System.out.print("Lutfen fizik notunuzu giriniz: ");
+        int fizik = input.nextInt();
+
+        System.out.print("Lutfen kimya notunuzu giriniz: ");
+        int kimya = input.nextInt();
+
+        System.out.print("Lutfen tarih notunuzu giriniz: ");
+        int tarih = input.nextInt();
+
+        System.out.print("Lutfen muzik notunuzu giriniz: ");
+        int muzik = input.nextInt();
+
+        double ortalama = (matematik + turkce + fizik + kimya + tarih + muzik) / 6;
+
+        System.out.println("Not ortalamaniz: " + ortalama);
+
+        boolean kosul = ortalama > 50;
+        System.out.println("Dersten " + (kosul == true ? "gectiniz" : "kaldiniz"));
+
+    }
+}
