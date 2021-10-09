fd1db658023c7d87d8a463cf814ffcb369e6c599
Cavit İbrahim Gökdoğan <70701456+cavitgokdogan@users.noreply.github.com>
2021-10-09 13:11:32 +0300

    Update hesapMakinesi.java

--- hesapMakinesi.java
+++ hesapMakinesi.java
@@ -26,7 +26,12 @@ public class hesapMakinesi {
                 System.out.println("Carpma isleminin sonucu: " + (ilkSayi * ikinciSayi));
                 break;
             case 4:
-                System.out.println("Bolme isleminin sonucu: " + (ilkSayi / ikinciSayi));
+                if (ikinciSayi == 0){
+                    System.out.println("Bolme islemi icin 0 gecerli bir sayi degildir.");
+                }
+                else{
+                    System.out.println("Bolme isleminin sonucu: " + (ilkSayi / ikinciSayi));
+                }
                 break;
         }
     }
